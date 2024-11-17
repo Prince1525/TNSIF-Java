cfa664896755abb3335704487419fffa8948026e
mehak <mehak@lapi>
2024-11-17 12:58:27 +0530

    Initial Commit

---
+++ HELLO/src/HELLO/Firstcode.java
@@ -0,0 +1,13 @@
+package HELLO;
+
+public class Firstcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		System.out.println("	Hello");
+		System.out.println("		Hello");
+	}
+
+}
